be30d01aee727d88eb5f1c99dab41ce37bd0d0c9
ayberkbalay <91059409+ayberkbalay@users.noreply.github.com>
2023-07-06 15:14:46 +0300

    update

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,10 +4,10 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>365 Technology Know-How</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/365-Technology',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/eSwqPeEM7',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
